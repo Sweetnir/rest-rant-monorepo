1a3221804d804d5fbbc0a099c3e9d64492dca90c
Damon Herrera <bustinbubs@gmail.com>
2023-07-29 10:34:24 -0700

    backend code

--- backend/index.js
+++ backend/index.js
@@ -11,12 +11,15 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+
 // Controllers & Routes
 
 app.use(express.urlencoded({ extended: true }))
 
+  
 app.use('/places', require('./controllers/places'))
 app.use('/users', require('./controllers/users'))
+app.use('/authentication', require('./controllers/authentication'))
 
 // Listen for Connections
 app.listen(process.env.PORT, () => {
